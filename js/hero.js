7179ea29512639bac6d2ebd3f086b0cf85af097f
Natalie Macias <nramirez787@gmail.com>
2017-01-05 20:37:59 -1000

    adding hero movement

--- js/hero.js
+++ js/hero.js
@@ -1,7 +1,9 @@
 ((Phaser, Game, CFG) => {
-  if(Game === undefined){
+
+  if( Game === undefined ){
     Game = window.Game = {};
   }
+
   const SCALE = 1;
   const MOVE_SPEED = 850;
   const JUMP_VELOCITY = 2950;
@@ -13,8 +15,8 @@
     JUMP_SPEED : 4,
   };
 
-  Game.Hereo = class {
-    constructor(game, x, y) {
+  Game.Hero = class{
+    constructor(game, x, y){
       this.game = game;
       this.sprite = this.game.add.sprite(x, y, CFG.ASSETS.GFX);
       this.sprite.scale.set(SCALE);
@@ -27,12 +29,52 @@
         jump : this.sprite.animations.add('jump', [ 'hero-jump-2.png', 'hero-jump-1.png' ]),
       };
 
-// allows passing through platforms
-this.sprite.body.checkCollision.up =
-this.sprite.body.checkCollision.left =
-this.sprite.body.checkCollision.right = false;
+      // allows passing through platforms
+      this.sprite.body.checkCollision.up =
+      this.sprite.body.checkCollision.left =
+      this.sprite.body.checkCollision.right = false;
 
-this.sprite.update = this.update.bind(this);
-    };
+      this.sprite.update = this.update.bind(this);
+    }
+
+    update(){
+      let hitPlatform = this.game.physics.arcade.collide(this.sprite, Game.platformsGroup);
+      let jumping = this.sprite.body.velocity.y !== 0;
+
+      this.sprite.body.velocity.x = 0;
+      if (Game.cursors.left.isDown) {
+        this.sprite.body.velocity.x = -MOVE_SPEED;
+        if(!jumping && this.sprite.animations.currentAnim !== this.animations.left){
+          this.animations.left.play(ANIMATIONS.LEFT_SPEED, true);
+        }
+      } else if (Game.cursors.right.isDown) {
+        this.sprite.body.velocity.x = MOVE_SPEED;
+        if(!jumping && this.sprite.animations.currentAnim !== this.animations.right){
+          this.animations.right.play(ANIMATIONS.RIGHT_SPEED, true);
+        }
+      } else if(!jumping){
+        //  Stand still
+        if(this.sprite.animations.currentAnim !== this.animations.idle){
+          this.animations.idle.play(ANIMATIONS.IDLE_SPEED, true);
+        }
+      }
+
+      //  Allow the player to jump if they are touching the ground.
+      if (Game.cursors.up.isDown && this.sprite.body.touching.down && hitPlatform)
+      {
+        this.sprite.body.velocity.y = -JUMP_VELOCITY;
+        if(this.sprite.animations.currentAnim !== this.animations.jump){
+          this.animations.jump.play(ANIMATIONS.JUMP_SPEED, false);
+        }
+      }
+
+      // wrap around stage
+      if(this.sprite.x > CFG.GAME_WIDTH){
+        this.sprite.x = -this.sprite.width;
+      } else if(this.sprite.x < -this.sprite.width){
+        this.sprite.x = CFG.GAME_WIDTH;
+      }
+    }
   };
-})(window.phaser, window.Game, window.Game.Configuration);
+
+})(window.Phaser, window.Game, window.Game.Configuration);
